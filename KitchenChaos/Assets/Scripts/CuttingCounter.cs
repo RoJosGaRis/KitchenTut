58f0cd874e76920a9f2eb8f77b9a2d237debf1a2
Roberto García <A01284731@tec.mx>
2023-06-28 21:53:01 -0600

    Applied restrictions to cutting andfinishedrecipes

--- KitchenChaos/Assets/Scripts/CuttingCounter.cs
+++ KitchenChaos/Assets/Scripts/CuttingCounter.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     public override void Interact(Player player) {
         if(!HasKitchenObject()){
             if(player.HasKitchenObject()){
+                if(HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
                 player.GetKitchenObject().SetKitchenObjectParent(this);
             } else {
 
@@ -23,9 +24,29 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if(HasKitchenObject()){
+        if(HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())){
+            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
             GetKitchenObject().DestroySelf();
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO){
+        
+        foreach(CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == inputKitchenObjectSO){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO){
+        foreach(CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == inputKitchenObjectSO){
+                return cuttingRecipeSO.output;
+            }
+        }
+        return null;
     }
 }
